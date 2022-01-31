c8226b95792bf77358fb1d4f62b355a45acb0ed2
Reina Khasanova <reina.khasanova@iaau.edu.kg>
2022-01-31 16:04:40 +0600

    injection into constructor

--- app/src/main/java/com/example/dependencyinjectionstart/example1/Activity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example1/Activity.kt
@@ -2,13 +2,5 @@ package com.example.dependencyinjectionstart.example1
 
 class Activity {
 
-    val monitor = Monitor()
-    val keyboard = Keyboard()
-    val mouse = Mouse()
-    val computerTower = ComputerTower(
-        Storage(),
-        Memory(),
-        Processor()
-    )
-    val computer = Computer(monitor, computerTower, keyboard, mouse)
+   val computer = Component().getComputer()
 }
